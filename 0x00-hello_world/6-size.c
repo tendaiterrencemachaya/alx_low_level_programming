f1f0161c42a1c78557907f0ec20f650a0e83c115
Tendai Terrence Machaya <tendai161221machaya@gmail.com>
2023-02-18 20:47:20 +0000

    awesome

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * prints size 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -13,3 +13,5 @@ printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
+
+
